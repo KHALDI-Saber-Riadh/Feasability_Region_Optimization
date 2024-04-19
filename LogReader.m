b96e54194757fddddaeeb5847ffac6b3cba9eabc
Saber Riadh Khaldi <saber-riadh.khaldi@lirmm.fr>
2024-04-19 17:01:46 +0200

    minors

--- LogReader.m
+++ LogReader.m
@@ -1,11 +1,11 @@
-CoM = readtable('com_log.csv');
+CoM = readtable('com_forward_log.csv');
 CoM = table2array(CoM);
 delRow = any(CoM==0,2);
 CoM(delRow,:) = [];
 clear delRow
 
-rightFoot = readtable('right_foot_log.csv');
+rightFoot = readtable('forward_right_foot_log.csv');
 rightFoot = table2array(rightFoot);
 
-leftFoot = readtable('left_foot_log.csv');
+leftFoot = readtable('forward_left_foot_log.csv');
 leftFoot = table2array(leftFoot);
